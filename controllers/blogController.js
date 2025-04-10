80d7d4f8e744e7d08b21becde4d277e5e0ae7754
Ilcho Dimeski <ilco_dimeski@live.co.uk>
2025-04-10 08:50:05 +0000

    10.04

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -55,19 +55,4 @@ export async function renderSinglePost(req, res) {
     html
   });
 }
-
-const posts = [
-    {
-      title: "Welcome to IkoConnect Blog!",
-      content: "Here you'll find tips, tutorials, and inspiration for your freelancing and remote work journey."
-    },
-    {
-      title: "Top 5 Freelance Platforms in 2025",
-      content: "Explore the best websites for getting freelance gigs this year, including Upwork, Fiverr, and more."
-    }
-  ];
-  
-  export function renderBlogPage(req, res) {
-    res.render("pages/blog", { posts });
-  }
   
